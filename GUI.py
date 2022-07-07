a3ef04a929f94d17f49a706efa90f50a9877d6ef
blue <codyjacobsen59@gmail.com>
2022-07-07 17:29:23 -0400

    added initialize.py

--- GUI.py
+++ GUI.py
@@ -8,18 +8,6 @@ class GUI:
         frame = Frame(master, width=500, height=500, bg="grey")
         frame.grid(rowspan=5, columnspan=5)
 
-        self.chromeBtn = Button(frame, text="Chrome")
-        self.firefoxBtn = Button(frame, text="Firefox")
-        self.edgeBtn = Button(frame, text="Microsoft Edge")
-        self.ieBtn = Button(frame, text="Internet Explorer")
-        self.operaBtn = Button(frame, text="Opera")
-
-        self.chromeBtn.grid(row=0, column=0, sticky=W)
-        self.firefoxBtn.grid(row=1, sticky=W)
-        self.edgeBtn.grid(row=2, sticky=W)
-        self.ieBtn.grid(row=3, sticky=W)
-        self.operaBtn.grid(row=4, sticky=W)
-
         self.quit = Button(frame, text="Quit", command=frame.quit)
         self.quit.grid(row=0, column=3, sticky=E)
 
